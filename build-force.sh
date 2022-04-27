2a599fd3d925ce4aef03830628633e87929d2bb0
Your Name <you@example.com>
2022-04-26 20:23:24 -0400

    Update on Tue Apr 26 20:23:24 EDT 2022
    Signed-off-by: Your Name <you@example.com>

--- build-force.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-rm -rf _site
-git clone git@github.com:joelagnel/joelfernandes.org.git _site -b gh-pages
-jekyll build
-cd _site
-git add *
-git commit -asm "Update on $(date)"
-git push origin HEAD:gh-pages
